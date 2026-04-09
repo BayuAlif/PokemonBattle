494c3ef8d34d7b7b5dae2e591f370c7380c67e9a
Jean <Jeankisekiwaluyo09@gmail.com>
2026-04-09 23:36:11 +0700

    menambah logic inventory, semoga benar, amin

--- src/Models/Inventory.java
+++ src/Models/Inventory.java
@@ -1,5 +1,51 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    // 1. Atribut sesuai banget sama class diagram (Private pakai minus '-')
+    private int id;
+    private Trainer trainer;
     
+    // Ini perwujudan garis panah 1..* ke Item
+    // Kita pakai 'Items' karena class abstract buatanmu namanya itu
+    private List<Items> daftarItem;
+
+    // 2. Constructor buat bikin objek Inventory
+    public Inventory(int id, Trainer trainer) {
+        this.id = id;
+        this.trainer = trainer;
+        this.daftarItem = new ArrayList<>(); // Awal mula tas kosong
+    }
+
+    // 3. Method + tambahItem(): void
+    // Di diagram ndak ada parameternya, tapi secara logika wajib ada barang yang dimasukin
+    public void tambahItem(Items itemBaru) {
+        this.daftarItem.add(itemBaru);
+        System.out.println("[Inventory] " + itemBaru.getName() + " dimasukkan ke tas.");
+    }
+
+    // 4. Method + hapusItem(): void
+    public void hapusItem(Items itemHapus) {
+        if (this.daftarItem.remove(itemHapus)) {
+            System.out.println("[Inventory] " + itemHapus.getName() + " dikeluarkan dari tas.");
+        } else {
+            System.out.println("[Inventory] Gagal! Item tidak ditemukan.");
+        }
+    }
+
+    // 5. Method + getItems(): List
+    public List<Items> getItems() {
+        return this.daftarItem;
+    }
+
+    // --- Getter tambahan (Biar aman kalau class lain mau ngecek ID/Trainer) ---
+    public int getId() {
+        return id;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
 }
